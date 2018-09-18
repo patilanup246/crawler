cea1b56d2024f0409b5f3f1cd8a7ac485459b361
stevedang <nguyendd@gmail.com>
2018-09-19 00:34:02 +0800

    all changes

--- crawler_startups.py
+++ crawler_startups.py
@@ -1,134 +1,97 @@
 #!python
 from selenium import webdriver
-from selenium.webdriver.remote.webdriver import WebDriver
+#from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests
-import time, sys, os
+import time, sys, os, datetime
 import codecs
 from io import open
 
 url='https://www.startupsg.net/directory/startups'
 
 test=True
-stop=1
-csvfile="page"
-time_out=2
-######################################################
+stop=10
+time_out=1
 folder="./raw/startups/"
+######################################################
 delim=","
 quotes="\""
 newline="\n"
 csvfile="page"
 pidfile="chrome.pid"
+WINDOW_SIZE = "1920,1080"
+IMPLICIT_WAIT=30
+usr="nguyendd+startupsg@gmail.com"
+pwd="o9Y1JcrE0E"
+########
+start_time=time.time()
+print(datetime.datetime.now())
+#######k
 options = webdriver.ChromeOptions()
-######################################################
-def attach_to_session():
-    with open(pidfile, "rb") as ins:
-        array = []
-        for line in ins:
-            array.append(line)
-        executor_url = array[0]
-        session_id = array[1]
-    print executor_url if test else None
-    print session_id if test else None
-    sys.stdout.flush()
-    original_execute = WebDriver.execute
-    def new_command_execute(self, command, params=None):
-        if command == "newSession":
-            # Mock the response
-            return {'success': 0, 'value': None, 'sessionId': session_id}
-        else:
-            return original_execute(self, command, params)
-    # Patch the function before creating the driver object
-    WebDriver.execute = new_command_execute
-    driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
-    driver.session_id = session_id
-    # Replace the patched function with original function
-    WebDriver.execute = original_execute
-    return driver
-########################################################
-
-
+options.add_argument("--window-size=%s" % WINDOW_SIZE)
 options.add_argument('headless')
-
-#browser = webdriver.Chrome(chrome_options=options)
-#browser.get(url)
-browser = attach_to_session()
-# Save the window opener (current window, do not mistaken with tab... not the same).
-main_window = browser.current_window_handle
-print("Main window: ",  main_window)
-sys.stdout.flush()
-count=1
+#######k
+driver = webdriver.Chrome(chrome_options=options)
+driver.get(url)
+driver.implicitly_wait(IMPLICIT_WAIT)
+time.sleep(time_out)
 try:
-    time.sleep(time_out)
-    for i in range(1,100000):
-        records = browser.find_elements_by_xpath('//div[@class="flex xs12"]/a')
-        #reach each line
-        print "number of record on this page: ", len(records) if test else None
-        sys.stdout.flush()
-        for record in records:
-            #name=record.text
-            new_url = record.get_attribute('href')
-            print new_url if test else None
-            sys.stdout.flush()
-            #click on the link
-            record.click()
-            time.sleep(time_out)
-            print("-1 " + browser.window_handles[-1])
-            print("0 " + browser.window_handles[0])
-            print("1 " + browser.window_handles[1])
-            print("Switing to " + browser.window_handles[1])
-            sys.stdout.flush()
-            #switch to the new window
-            browser.switch_to_window(browser.window_handles[1])
-            #browser.switch_to.window(browser.window_handles[1])
-            print("Sub window: ",  browser.title)
-            sys.stdout.flush()
-            #r = requests.get(new_url, verify=False)
-            time.sleep(time_out)
-            r = browser.page_source
-            datafile=folder+csvfile+str(count)+".html"
-            count+=1
-            # with open(datafile, "a", encoding='utf-8') as output:
-                # output.write(r)
-                # output.flush
-            #switch to the new window'
-            time.sleep(time_out)
-            #browser.close()
-            #browser.switch_to.window(browser.window_handles[0])
-            # record.click()#click throug the link
-            # time.sleep(time_out)
-            # #switch to the new tab
-            # browser.switch_to.window(browser.window_handles[1])
-            # datafile=folder+csvfile+str(count)+".html"
-            # count+=1
-            # with open(datafile, "a", encoding='utf-8') as output:
-                # data = browser.page_source
-                # output.write(data)
-                # output.flush
-            # #close the new tab
-            # time.sleep(time_out)
-            # browser.close()
-            # #go back to the original tab
-            # browser.switch_to.window(browser.window_handles[0])
-            # #for testing purpose
-        buttons = browser.find_elements_by_xpath('//button[@class="pagination__navigation"]')
-        if len(buttons)==0:
-            break 
-        button=buttons[0]
-        if button.is_enabled():
-            button.click()    
-        else:
-            break
-        time.sleep(time_out)
-        if test and i==stop:
-            break
+	#login into the app
+	#driver.save_screenshot("output.png")
+	#raise Exception("Testing")
+	e = driver.find_element_by_css_selector('#app > div.application--wrap > div > div.header > nav > div > a')
+	#raise Exception("Testing")
+	e.click()
+	time.sleep(time_out)
+	#
+	elem = driver.find_element_by_css_selector('#app > div.dialog__content.dialog__content__active > div > div.login-form.entity-form > div > form > div:nth-child(1) > div.input-group__input > input[type="text"]')
+	elem.send_keys(usr)
+	#
+	elem = driver.find_element_by_css_selector('#app > div.dialog__content.dialog__content__active > div > div.login-form.entity-form > div > form > div:nth-child(2) > div.input-group__input > input[type="password"]')
+	#print elem
+	elem.send_keys(pwd)
+	elem.send_keys(Keys.RETURN)
+	time.sleep(time_out)
+	#time.sleep(time_out)
+	#get link one by one
+	page=1
+	count=1
+	while True:
+		records = driver.find_elements_by_xpath('//div[@class="flex xs12"]/a')
+		#print(len(records))
+		for record in records:
+			#print(record.get_attribute('href'))
+			sys.stdout.flush()
+			record.click()
+			time.sleep(time_out)
+			driver.switch_to.window(driver.window_handles[-1])
+			r = driver.page_source
+			datafile=folder+csvfile+str(count)+".html"
+			count+=1
+			with open(datafile, "w", encoding="utf-8") as output:
+				output.write(r)
+				output.close()
+			#time.sleep(time_out)
+			driver.close()
+			driver.switch_to.window(driver.window_handles[0])
+		if test and page==stop:
+			break	
+		page+=1
+		driver.find_elements_by_xpath('//button[@class="pagination__navigation"]')[0].click()
 except Exception as e:
-    print("Exception: " + str(e))
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    print(exc_type, fname, exc_tb.tb_lineno)
-#finally:
-    #browser.close()
+	print(str(e))
+	exc_type, exc_obj, exc_tb = sys.exc_info()
+	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+	print(exc_type, fname, exc_tb.tb_lineno)
+	driver.save_screenshot("screenshot.png")
+finally:
+	print(datetime.datetime.now())
+	end_time=time.time()
+	print("Total processing time: ",(end_time-start_time))
+	print("Total number of pages: ", page)
+	print("Total number of companies: ", count)
+	driver.quit()
 
