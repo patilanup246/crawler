e69ff1c2a81539d537f732c9536f904136b72f63
Steve Dang <nguyendd@gmail.com>
2018-09-20 14:05:00 +0800

    get the correct result

--- get_startups.py
+++ get_startups.py
@@ -1,3 +1,5 @@
+#!python
+# -*- coding: utf-8 -*-
 import os, glob, csv
 from bs4 import BeautifulSoup
 import re,  codecs
@@ -27,90 +29,75 @@ with open(OUT_FILENAME,'w',encoding="utf-8" ) as out_file:
 		filename=FOLDER+IN_FILENAME+str(i)+FILE_EXTENSION
 		datarow=[]
 		datarow.append(str(i))
-		with open(filename, encoding="utf-8") as html_file:
+		with open(filename, 'r', encoding="utf-8") as html_file:
 			soup=BeautifulSoup(html_file,'html.parser')
 			sections=soup.find_all('section')
 			print(len(sections)) 
 			if len(sections)<3: 
 				break
-			#general=sections[0].text.encode("utf-8")
+			#heading
 			general=sections[0].find_all("section", {"id":"general"})[0]
+			print("=============header information")
 			heading=general.find_all("div",{"class":"flex md9 column"})[0]
+			print(heading.encode("utf-8"))
 			list1=[]
-			for text1 in heading.text.strip().split(NEWLINE):
-				list1.append(text1.encode("utf-8").strip())
-			for text1 in list1:
-				print(text1)
+			for text1 in heading.select('div'):
+				print(text1.text.encode("utf-8"))
+			#print(heading.encode("utf-8"))
+			#for text1 in heading.text.encode("utf-8").strip().split(NEWLINE):
+				#list1.append(text1.encode("utf-8").strip())
+				#list1.append(text1)
+			#for text1 in list1:
+			#	print(text1)
+				#datarow.append(text1)
+			#datarow = datarow + list1
+			#body details
+			print("=============body details information")
 			body=general.find_all("div",{"class":"flex entity__details xs12"})[0]
-			details=body.find_all("div",{"class":"flex section__legend xs12 md3"})
-			for text1 in details:
-				print(text1.text)
-	
-			#print(",".join(list1))
-			#print(heading.text.encode("utf-8").strip())
-			team=sections[0].find_all("section", {"id":"team"})[0].find_all("h4")
-			print("leng team: ",len(team))
-			for text1 in team[1:]:
-				print(text1.text.strip(), text1.next_sibling.text.strip())
+			#for text1 in body.select('div[class*="xs12 md9"]'):
+			#	print(text1.get_text().strip().encode("utf-8"))
+			for text1 in body.select('div[class*="layout wrap"]')[:-1]:
+				datarow.append(text1.find_all('div')[1].text.encode("utf-8").strip())
+			#datarow = datarow + body.select('div[class*="layout wrap"]')[:-1]
+			#print(body.select('div[class*="read-more__content"]')[0].get_text().encode("utf-8"))
+			datarow.append(body.select('div[class*="read-more__content"]')[0].get_text().encode("utf-8"))
+			#contact information
+			print("=============contact details information")
+			contact=general.find_all("div",{"class":"contact card"})[0]
+			datarow.append(contact.select('li[class*="address"]')[0].get_text().encode("utf-8"))
+			datarow.append(contact.select('li[class*="website"]')[0].get_text().strip())
+			datarow.append(contact.select('li[class*="email"]')[0].get_text().strip())
+			datarow.append(contact.select('li[class*="phone"]')[0].get_text().strip())
+			#team information
+			print("=============team information")
+			team=sections[0].find_all("section", {"id":"team"})[0].select('h4[class*="team"]')
+			team1=sections[0].find_all("section", {"id":"team"})[0]
+			team_text=""
+			for text1 in team:
+				team_text += text1.text.strip() + ","
+				team_text += text1.next_sibling.text.strip() + "\r\n"
+			datarow.append(team_text)
 			general=sections[0].text
 			#print(general.encode("utf-8"))
+			#funding information
+			print("=============funding information")
 			funding=sections[0].find_all("section", {"id":"funding"})[0].find_all("div",{"class":"section__sub-legend"})
 			for text1 in funding:
-				print(text1.text.strip(),  text1.next_sibling.text.strip())
+				datarow.append(text1.next_sibling.text.strip())
+			#funding raised information
+			print("=============funding raised information")
 			funding_raised=sections[0].find_all("section", {"id":"funding-raised"})[0].find_all("tr",{"class":"investment-round hasToggle"})
+			list1=[]
 			for text1 in funding_raised:
-				list1=[]
+				list2=[]
 				for text2 in text1.find_all("div")[:-1]:
-					list1.append(text2.text.strip())
-				print(",".join(list1))
-			#name=top_table.find('h1')
-			#print name.text.replace('\r',' ').replace('\n',' ').strip()
-			#datarow.append(name.text.replace('\r',' ').replace('\n',' ').strip())
-
-			###############
-			#row1=top_table.find_all('p',{'class':'control-group'})[2]
-			#spans=row1.find_all('span')
-			#list1=[]
-			#for span in spans:
-			#	span_text = span.text.replace('\r',' ').replace('\n',' ').strip()
-			#	#span_text=span.text.encode('utf8','ignore').replace('\r',' ').replace('\n',' ').strip()
-			#	#span_text=span_text.replace('\xc2',' ').replace('\xa0',' ').strip()
-			#	list1.append(span_text)		
-			#list1 = filter(None,list1)
-			#address=" ".join(list1)
-			##print address
-			#datarow.append(address)
-			###############
-			#row2=row1.findNext('div',{'class':'col-md-6'})
-			#list1=[]
-			##for detail in details.strings:
-			#for detail in row2.findAll(text=True):
-			#	#detail=detail.encode('utf8','ignore').replace('\r',' ').replace('\n',' ').replace('\xc2',' ').replace('\xa0',' ').strip()
-			#	detail=detail.replace('\r',' ').replace('\n',' ').replace('\xc2',' ').replace('\xa0',' ').strip()
-			#	list1.append(detail)
-			#list1= filter(None,list1)
-			#datarow+=list1
-			##datarow.append(",".join(list1))
-			###############
-			#row3=row2.findNext('div')
-			#contact=row3.text.replace('\r','').replace('\n','')
-			#datarow.append(contact)
-			##print repr(contact)
-			#row5=top_table.findAll('div',{'class':'row'})[5]
-			#details=row5.findAll('span',{'class':'data-font'})
-			#list1=[]
-			#for detail in details:
-			#	list1.append(detail.text)
-			#list1= filter(None,list1)
-			##print ",".join(list1)
-			##print list1
-			##datarow=datarow + list1
-			#datarow.append(",".join(list1))
-			###################print list1
-			#row6=top_table.findAll('div',{'class':'row'})[6].findChildren('td')[0]
-			#profile=row6.text.replace('\n',' ').replace('\r',' ').strip()
-			#profile=row6.text.replace("-","-").encode('utf8').replace("\\xe2\\x80\\x99","'").replace('\r',' ').replace('\n',' ')
-			#datarow.append(profile)
+					list2.append(text2.text.strip())
+				list1.append(",".join(list2))
+			datarow.append("\r\n".join(list1))
+			
+		print("============final result")
+		for text1 in datarow:
+			print(text1.encode("ascii"))
 		print(datarow)
 		writer.writerow(datarow)
 		if TEST and i==stop:
