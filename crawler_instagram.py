f5babadb2a3cc23b3cb72f99c35ef3f63ebf0b4b
Steve Dang <nguyendd@gmail.com>
2019-01-18 09:37:31 +0800

    Revert "test"
    This reverts commit ef512f67e3894bfc78c8dd02b2e85d55632e6678.

--- crawler_instagram.py
+++ crawler_instagram.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import requests, csv, sys, re, argparse, random,time, os
+import requests, csv, sys, re, argparse, random,time
 import time, glob, urllib3
 from bs4 import BeautifulSoup
 from collections import defaultdict
@@ -20,58 +20,56 @@ result_file = "./results/instagram.csv"
 options = webdriver.ChromeOptions()
 options.add_argument("--window-size=%s" % WINDOW_SIZE)
 options.add_argument("--start-maximized")
-options.add_argument('--headless')
+#options.add_argument('--headless')
 
 categories_list = defaultdict(list) 
 ######################################
 def getAllCategories():
-    url = first_url
-    print(url)
-    sys.stdout.flush()
-    headers= {
-          'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-          'accept-encoding':'gzip, deflate, sdch, br',
-          'accept-language':'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
-          'cache-control':'max-age=0',
-          'upgrade-insecure-requests':'1',
-          'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
-    }
-    response = requests.get(url,headers=headers, verify=False)
-    print(response.status_code)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    categories = soup.select('div[id*="category"]')
-    for category in categories:
-        #print(category)
-        cat = category.select('h3[class*="title"]')[0].get_text().replace('\n','').strip()
-        #print(header)
-        links = category.select('a[class*="category"]')
-        for link in links:
-            temp = link.attrs["href"]
-            #print(temp)
-            categories_list[temp].append(cat)
-    #print(categories_list)
+	url = first_url
+	print(url)
+	sys.stdout.flush()
+	headers= {
+		  'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		  'accept-encoding':'gzip, deflate, sdch, br',
+		  'accept-language':'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
+		  'cache-control':'max-age=0',
+		  'upgrade-insecure-requests':'1',
+		  'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+	}
+	response = requests.get(url,headers=headers, verify=False)
+	print(response.status_code)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	categories = soup.select('div[id*="category"]')
+	for category in categories:
+		#print(category)
+		cat = category.select('h3[class*="title"]')[0].get_text().replace('\n','').strip()
+		#print(header)
+		links = category.select('a[class*="category"]')
+		for link in links:
+			temp = link.attrs["href"]
+			#print(temp)
+			categories_list[temp].append(cat)
+	#print(categories_list)
 ######################################
 def parse(url):
     print(url)
-    sys.stdout.flush()
-    country = ""
-    driver = webdriver.Chrome(chrome_options=options)
-    try:
-        driver.get(url)
-        #driver.implicitly_wait(IMPLICIT_WAIT)
-        time.sleep(random.randint(1,2))
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        location = ""
-        locations= soup.select('a[href*="location"]')
-        if len(locations)>0:
-            country = locations[0].get_text()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-    finally:
-        driver.quit()
-    return country
+	headers= {
+		  'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		  'accept-encoding':'gzip, deflate, sdch, br',
+		  'accept-language':'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
+		  'cache-control':'max-age=0',
+		  'upgrade-insecure-requests':'1',
+		  'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+	}
+	response = requests.get(url,headers=headers, verify=False)
+	print(response.status_code)
+	soup = BeautifulSoup(response.text, 'html.parser')
+    location = ""
+	locations= soup.select('a[href*="/explore/location"]')
+    if len(locations)>0:
+        location = locations[0].get_text()
+    print(location)
+    
 ######################################
 def appendToFile(datarow):
     with open(result_file, 'a', encoding = 'utf-8') as output:
@@ -81,29 +79,25 @@ def appendToFile(datarow):
 if __name__=="__main__":
     argparser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     argparser.add_argument('action',help= 'test, parse, crawl')
-    argparser.add_argument('--file',nargs='?',help= 'crawl --file <filename>')
-    argparser.add_argument('--output',nargs='?',help= 'crawl --output<filename>')
+    argparser.add_argument('--zipcode',nargs= '?', help = 'crawl --zipcode <zipcode>')
+    argparser.add_argument('--file',nargs= '?', help = 'crawl --file <filename>')
+    argparser.add_argument('--output',nargs= '?', help = 'crawl --output <filename>')
+    argparser.add_argument('--category',nargs= '?', help = 'crawl --category <category>')
     args = argparser.parse_args()
     action = args.action
-    url_file = args.file
-    output = args.output
+    zipcode= args.zipcode
+    category= args.category
+    file= args.file
+    output= args.output
     print ("action: %s"%(action))
-    print ("file: %s"%(url_file))
-    print ("output: %s"%(output))
+    print ("zipcode:",zipcode)
+    print ("file:",file)
+    print ("output:",output)
     sys.stdout.flush()
     if not output is None:
         result_file = output
+    if "test" == action:
+        getAllCategories()
     if "parse" == action:
         parse("https://instagram.com/p/BshS-84FQY3")
-    if "crawl" == action:
-        full_list = []
-        with open(url_file, 'r') as input_file:
-            reader = csv.reader(input_file)
-            full_list= list(reader)
-        for list1 in full_list:
-            url = list1[6].strip()
-            if url and "http" in url:
-                country = parse(url)
-                if country != "Directory":
-                    list1[9] = country
-            appendToFile(list1)
+
