92179dd80581cee16df6a46cbb2277035576f9c3
Steve Dang <nguyendd@gmail.com>
2019-02-28 18:41:32 +0800

    new run.sh

--- instagram_posts_crawler/run.sh
+++ instagram_posts_crawler/run.sh
@@ -1,10 +1,13 @@
-./crawler.py posts -u bmwmotorsport -o ./bmwmotorsport.csv
-./crawler.py posts -u dstecheetah -o ./dstecheetah.csv
-./crawler.py posts -u fiaformulae -o ./fiaformulae.csv 
-./crawler.py posts -u geoxdragon -o ./geoxdragon.csv 
-./crawler.py posts -u hwaag_official -o ./hwaag_official.csv 
-./crawler.py posts -u jaguarracing -o ./jaguarracing.csv 
-./crawler.py posts -u mahindraracing -o ./mahindraracing.csv 
-./crawler.py posts -u nioformulae -o ./nioformulae.csv 
-./crawler.py posts -u nissanedams -o ./nissanedams.csv 
-./crawler.py posts -u venturi_fe -o ./venturi_fe.csv 
+#./crawler.py posts -u bmwmotorsport -o ./bmwmotorsport.csv
+#./crawler.py posts -u dstecheetah -o ./dstecheetah.csv
+#./crawler.py posts -u fiaformulae -o ./fiaformulae.csv 
+#./crawler.py posts -u geoxdragon -o ./geoxdragon.csv 
+#./crawler.py posts -u hwaag_official -o ./hwaag_official.csv 
+#./crawler.py posts -u jaguarracing -o ./jaguarracing.csv 
+#./crawler.py posts -u mahindraracing -o ./mahindraracing.csv 
+#./crawler.py posts -u nioformulae -o ./nioformulae.csv 
+#./crawler.py posts -u nissanedams -o ./nissanedams.csv 
+#./crawler.py posts -u venturi_fe -o ./venturi_fe.csv 
+./crawler_post_instagram.py --url_file fiaformulae.csv
+./crawler_post_instagram.py --url_file mahindraracing.csv
+./crawler_post_instagram.py --url_file nioformualae.csv
